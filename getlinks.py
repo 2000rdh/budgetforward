ee870a051592555baa2a0934917cf79d4bdf3586
2000rdh <43261008+2000rdh@users.noreply.github.com>
2021-02-21 09:10:21 -0500

    Update getlinks.py

--- getlinks.py
+++ getlinks.py
@@ -3,25 +3,18 @@ from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
 import csv
 
-# init the colorama module
-
-# initialize the set of links (unique links)
 external_urls = []
 
 total_urls_visited = 0
 
 def is_valid(url):
-    """
-    Checks whether `url` is a valid URL.
-    """
+
     parsed = urlparse(url)
     return bool(parsed.netloc) and bool(parsed.scheme)
 
 
 def get_all_website_links(url):
-	"""
-	Returns all URLs that is found on `url` in which it belongs to the same website
-	"""
+
 	# all URLs of `url`
 	# domain name of the URL without the protocol
 	domain_name = urlparse(url).netloc
@@ -65,23 +58,6 @@ def get_all_website_links(url):
 	                external_urls.append(href)
 
 	        continue
-
-
-# def crawl(url, max_urls=50):
-#     """
-#     Crawls a web page and extracts all links.
-#     You'll find all links in `external_urls` and `internal_urls` global set variables.
-#     params:
-#         max_urls (int): number of max urls to crawl, default is 30.
-#     """
-#     global total_urls_visited
-#     total_urls_visited += 1
-#     links = get_all_website_links(url)
-#     for link in links:
-#         if total_urls_visited > max_urls:
-#             break
-#         crawl(link, max_urls=max_urls)
-
 
 if __name__ == "__main__":
     import argparse
